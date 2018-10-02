b62df973c5c388a18ef0ab0fcdd9fab672bef710
swakita14 <swakita14@wou.edu>
2018-10-01 17:26:20 -0700

    checking if Javascript is working

---
+++ HW2/script.js
@@ -0,0 +1,15 @@
+//Makes sure that the page is ready and loaded properly
+ $(document).ready(function(){
+
+
+ 		//Just a test message to see if everything ran correctly
+        console.log("Loaded Successfully");
+
+
+         
+
+
+
+          });
+
+
