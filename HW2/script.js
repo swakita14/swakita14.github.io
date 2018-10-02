54717aaa645fab46203b992e482a7d9e99d36945
swakita14 <swakita14@wou.edu>
2018-10-01 17:27:16 -0700

    assigned user input to variables

--- HW2/script.js
+++ HW2/script.js
@@ -5,11 +5,21 @@
  		//Just a test message to see if everything ran correctly
         console.log("Loaded Successfully");
 
+        //Form submission function
+        $('#myForm').submit(function(e){
+          e.preventDefault();
+
+          //Takes in all the inputs from the form and assigns it so we can do some calculation with it
+          var b_weight = $('#body-weight').val();
+          var w_lifted = $('#weight-lifted').val();
+          var rep = $('#repetition').val();
+
 
          
 
+ 
 
 
           });
 
-
+});
