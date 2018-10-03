61f3c9ccc8a81d1d3b742134cf281f25089d7041
swakita14 <swakita14@wou.edu>
2018-10-03 14:59:07 -0700

    Added Node class

--- HW3/translate-javacode/translate-javacode/Program.cs
+++ HW3/translate-javacode/translate-javacode/Program.cs
@@ -12,4 +12,18 @@ namespace translate_javacode
         {
         }
     }
+
+    class Node<T>
+    {
+        public T data;
+        public Node<T> next;
+
+
+        public Node(T data, Node<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+    }
 }
