1d53697a2cbe2f479da37ad69ebe735c4e80d671
swakita14 <swakita14@wou.edu>
2018-10-07 15:49:20 -0700

    Added XML comments for QueueUnderflowException class

--- HW3/translate-javacode/translate-javacode/Program.cs
+++ HW3/translate-javacode/translate-javacode/Program.cs
@@ -55,13 +55,25 @@ namespace translate_javacode
 
     }
 
+
+    /// <summary>
+    ///  A custom unchecked exception to represent situations where an illegal operation was performed on an empty queue.
+    ///  This QueueUnderflowException class extends the SystemExcpetions 
+    /// </summary>
     class QueueUnderflowException : SystemException
     {
+        /// <summary>
+        /// This overrides the extended SystemException class's constructor using the :base()
+        /// </summary>
         public QueueUnderflowException() : base()
         {
             
         }
 
+        /// <summary>
+        /// This overrides the extended SystemException class's constructor using the :base() and takes a msg
+        /// <paramref name="msg"/> takes in a message and overrides it through the SystemsExceptions constructor
+        /// </summary>
         public QueueUnderflowException(string msg) : base(msg)
         {
 
