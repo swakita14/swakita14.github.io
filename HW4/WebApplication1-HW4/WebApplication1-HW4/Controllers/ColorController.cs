4eab007fd1cbe6748b5c6d09347c7d7a0ae9744a
swakita14 <swakita14@wou.edu>
2018-10-13 15:40:20 -0700

    switched controllers from home to color

---
+++ HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/ColorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Web.Mvc;
+
+namespace WebApplication1_HW4.Controllers
+{
+    public class ColorController : Controller
+    {
+        // GET: Color
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ColorChooser(string color1, string color2)
+        {
+            ViewBag.trace = false;
+            if (color1 != null)
+            {
+                ViewBag.trace = true;
+            }
+            Color _color = System.Drawing.ColorTranslator.FromHtml(color1);
+            ViewData["color"] = _color;
+            return View();
+
+        }
+    }
+}
