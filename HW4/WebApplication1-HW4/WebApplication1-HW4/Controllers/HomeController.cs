0bae3a231660a2d34ac863c971200f3840f2bf35
swakita14 <swakita14@wou.edu>
2018-10-12 23:02:59 -0700

    added new actionresult, view page

--- HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/HomeController.cs
+++ HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace WebApplication1_HW4.Controllers
 
             return View();
         }
+
+        public ActionResult ColorChooser()
+        {
+            return View();
+        }
     }
 }
