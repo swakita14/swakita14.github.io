66be77d6c669ba2ae0c6f154dfc56a78d003c585
swakita14 <swakita14@wou.edu>
2018-10-14 22:14:35 -0700

    Finished the color chooser

--- HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/ColorController.cs
+++ HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/ColorController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Windows.Forms;
 using System.Drawing;
 using System.Web.Mvc;
+using System.Drawing.Printing;
 
 namespace WebApplication1_HW4.Controllers
 {
@@ -18,26 +21,111 @@ namespace WebApplication1_HW4.Controllers
         [HttpGet]
         public ActionResult ColorChooser()
         {
+            ViewBag.show = false;
             return View();
         }
 
         [HttpPost]
         public ActionResult ColorChooser(string color1, string color2)
         {
+
             color1 = Request.Form["color-input1"];
             color2 = Request.Form["color-input2"];
 
-            ViewBag.show = false;
-            if (color1 != null)
+            Color rgb_color1 = ColorTranslator.FromHtml(color1);
+            Color rgb_color2 = ColorTranslator.FromHtml(color2);
+
+            int final_A;
+            int final_R;
+            int final_B;
+            int final_G;
+
+            if (rgb_color1.A + rgb_color2.A >= 255)
             {
-                Color color = ColorTranslator.FromHtml(color1);
-                ViewBag.show = true;
-                ViewBag.test = color1;
-                ViewData["test"] = color;
+                final_A = 255;
             }
-            return View();
+            else
+            {
+                final_A = rgb_color1.A + rgb_color2.A;
+            }
+
+            if (rgb_color1.R + rgb_color2.R >= 255)
+            {
+                final_R = 255;
+            }
+            else
+            {
+                final_R = rgb_color1.R + rgb_color2.R;
+            }
+
+            if (rgb_color1.B + rgb_color2.B >= 255)
+            {
+                final_B = 255;
+            }
+            else
+            {
+                final_B = rgb_color1.B + rgb_color2.B;
+            }
+
+            if (rgb_color1.G + rgb_color2.G >= 255)
+            {
+                final_G = 255;
+            }
+            else
+            {
+                final_G = rgb_color1.G + rgb_color2.G;
+            }
+
+
+
+            string finale = ColorTranslator.ToHtml(Color.FromArgb(final_A, final_R, final_G, final_B));
+
+            //string finalcolor = color1 + color2;
+
+            
+            if (color1 != null && color2!=null)
+            {
+
+                ViewBag.show = true;
+
+                Rectangle myRectangle1 = new Rectangle(0,0 ,100, 100);
+                Rectangle myRectangle2 = new Rectangle(0, 0, 100, 100);
+
+                Rectangle newShape = new Rectangle(0, 0, 100, 100);
+
+                ViewBag.shape = "width:75px; height: 75px; border: 1px solid #000; background:" + color1 + "; ";
+                ViewBag.shape2 = "width:75px; height: 75px; border: 1px solid #000; background: " + color2 + "; ";
+                ViewBag.shape3 = "width:75px; height: 75px; border: 1px solid #000; background: " + finale + "; ";
+
+
+
+            }
+                return View();
+
         }
 
-       
+        private Color GetRgb(double r, double g, double b)
+        {
+            return Color.FromArgb(255, (byte)(r * 255.0), (byte)(g * 255.0), (byte)(b * 255.0));
+        }
+
+        public void FromHtml_Example(PaintEventArgs e)
+        {
+            // Create a string representation of an HTML color.
+            string htmlColor = "Blue";
+
+            // Translate htmlColor to a GDI+ Color structure.
+            Color myColor = ColorTranslator.FromHtml(htmlColor);
+
+            // Fill a rectangle with myColor.
+            e.Graphics.FillRectangle(new SolidBrush(myColor), 0, 0,
+                100, 100);
+        }
+
+
+
+
+
+
     }
 }
