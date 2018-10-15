6e199cabec45a7027262b8c43ca3a314339a3ece
swakita14 <swakita14@wou.edu>
2018-10-15 15:12:06 -0700

    small changes

--- HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/HomeController.cs
+++ HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/HomeController.cs
@@ -1,33 +1,67 @@
 ﻿using System;
-using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace WebApplication1_HW4.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        /// <summary>
+        /// This gets the user input and outputs the result. Using query strings takes in miles and returns metric units
+        /// </summary>
+        /// <returns>returns the view page of the converter.</returns>
+        [HttpGet]
+        public ActionResult Converter()
         {
+            //Takes in user input miles
+            double miles = Convert.ToInt32(Request.QueryString["miles"]);
+
+            //this recognizes the value of the radio button that the user has selected, in order to see which unit we are converting to
+            string metricUnit = Request.QueryString["metric-unit-radio"];
+            double milesToMetric = 0;
+            string unit = "";
+            ViewBag.Show = false;
+
+            //radion button value checking
+            if (metricUnit == "mm")
+            {
+                unit = "mm";
+                milesToMetric = (miles * 1609344);
+                ViewBag.Show = true;
+
+            }
+            else if (metricUnit == "cm")
+            {
+                unit = "cm";
+                milesToMetric = (miles * 160934.4);
+                ViewBag.Show = true;
+
+
+            }
+            else if (metricUnit == "mt")
+            {
+                unit = "m";
+                milesToMetric = (miles * 1609.344);
+                ViewBag.Show = true;
+
+            }
+            else if (metricUnit == "km")
+            {
+                unit = "km";
+                milesToMetric = (miles * 1.609344);
+                ViewBag.Show = true;
+            }
+
+            //output the results of the convertion
+            ViewData["results"] = (miles + " miles is equal to " + milesToMetric + unit);
+
+
+
+
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-      
     }
 }
