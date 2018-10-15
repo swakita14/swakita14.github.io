014aeb1e8ca7d633b95d12f17da904c863141808
swakita14 <swakita14@wou.edu>
2018-10-15 15:14:11 -0700

    small touches

--- HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/HomeController.cs
+++ HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/HomeController.cs
@@ -8,25 +8,7 @@ namespace WebApplication1_HW4.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
+   
 
         [HttpGet]
         public ActionResult Converter()
