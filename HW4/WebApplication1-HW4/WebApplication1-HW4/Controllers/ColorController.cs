c87d89b89a9f67e886627ba6391733e05ffc32cf
swakita14 <swakita14@wou.edu>
2018-10-18 13:21:54 -0700

    Done with HW4

---
+++ HW4/WebApplication1-HW4/WebApplication1-HW4/Controllers/ColorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Web.Mvc;
+using System.Drawing.Printing;
+
+namespace WebApplication1_HW4.Controllers
+{
+    public class ColorController : Controller
+    {
+        // GET: Color
+        public ActionResult Index()
+        {
+            return View("Hello World");
+        }
+
+       
+
+
+
+
+    }
+}
