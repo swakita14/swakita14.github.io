21f94ff64a5908790d307a3b83bbbbdf0fc1b549
Shion Wakita <swakita14@wou.edu>
2018-10-20 18:59:42 -0700

    fixed up.sql, created localdb

--- HW5/Campus-Applications/Campus-Applications/App_Data/up.sql
+++ HW5/Campus-Applications/Campus-Applications/App_Data/up.sql
@@ -12,8 +12,8 @@ CREATE TABLE dbo.Requests (
 );
 
 INSERT INTO dbo.Requests(FirstName, LastName, ApartmentName, Explanation, UnitNumber, SignedDate) VALUES
-    ('Shion' 'Wakita', 'Catron Houses', 'Leaking water pipe', '408A', '2018-10-20 10:23:00'),
-    ('Mashu' 'Wakita', 'Monmouth Townhouse', 'Heater not working', '374B', '2018-11-20 12:25:30'),
-    ('John' 'Stephens', 'University Houses', 'Crack in the Wall', '342C', '2018-09-24 14:23:10'),
-    ('Noah' 'Mendoza', 'Catron Houses', 'Broken refrigerator', '12D', '2018-07-13 10:24:00'),
-    ('Mark' 'Stemple', 'Independence Housing', 'Black widow breakout', '225A', '2018-05-20 08:23:22'),
+    ('Shion', 'Wakita', 'Catron Houses', 'Leaking water pipe', '408A', '2018-10-20 10:23:00'),
+    ('Mashu', 'Wakita', 'Monmouth Townhouse', 'Heater not working', '374B', '2018-11-20 12:25:30'),
+    ('John', 'Stephens', 'University Houses', 'Crack in the Wall', '342C', '2018-09-24 14:23:10'),
+    ('Noah', 'Mendoza', 'Catron Houses', 'Broken refrigerator', '12D', '2018-07-13 10:24:00'),
+    ('Mark', 'Stemple', 'Independence Housing', 'Black widow breakout', '225A', '2018-05-20 08:23:22');
