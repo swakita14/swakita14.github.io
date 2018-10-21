154c3254805305019e6328ae2d0fed08022ac190
Shion Wakita <swakita14@wou.edu>
2018-10-21 00:37:17 -0700

    Added model file and dbContext

---
+++ HW5/Campus-Applications/Campus-Applications/DAL/RequestdbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Campus_Applications.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Campus_Applications.DAL
+{
+    public class RequestdbContext : DbContext
+    {
+
+        public RequestdbContext() : base("name=RequestdbContext")
+        {
+
+        }
+
+        public virtual DbSet<Request> Requests { get; set; }
+    }
+}
---
+++ HW5/Campus-Applications/Campus-Applications/Models/Request.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Campus_Applications.Models
+{
+    public class Request
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string ApartmentName { get; set;}
+
+        [Required]
+        public string Explanation { get; set; }
+
+        [Required]
+        public int UnitNumber { get; set; }
+
+        public DateTime SignedDate { get; set; }
+    }
+}
