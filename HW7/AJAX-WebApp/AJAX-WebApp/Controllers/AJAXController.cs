e594764324d59a0675a376e9d2860935b1be8087
Shion Wakita <swakita14@wou.edu>
2018-11-03 16:59:10 -0700

    Initialized Controller and View

---
+++ HW7/AJAX-WebApp/AJAX-WebApp/Controllers/AJAXController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AJAX_WebApp.Controllers
+{
+    public class AJAXController : Controller
+    {
+        // GET: AJAX
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
