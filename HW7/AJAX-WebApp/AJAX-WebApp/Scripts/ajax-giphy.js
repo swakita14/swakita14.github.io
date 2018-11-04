8707a466188ab383d5d50a0a80766426d4ad3b4d
Shion Wakita <swakita14@wou.edu>
2018-11-03 17:39:31 -0700

    Added own js file and added to the index View

---
+++ HW7/AJAX-WebApp/AJAX-WebApp/Scripts/ajax-giphy.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#text-input').bind('keypress', function (e) {
+        if (e.which === 32) {//space bar
+            alert('space');
+        }
+      
+    });
+});
