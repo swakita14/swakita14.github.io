5539898d7681148c2d08cd8a3d67f003e5bbdddd
Shion Wakita <swakita14@wou.edu>
2018-11-11 17:28:51 -0800

    added initialized model file

---
+++ HW8/AuctionHouse/AuctionHouse/Models/Buyer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace AuctionHouse.Models
+{
+    public class Buyer
+    {
+      
+    }
+}
