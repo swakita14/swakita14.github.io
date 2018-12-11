e777a9678b8ceadc620405f9fe01e629f7d999c5
Shion Wakita <swakita14@wou.edu>
2018-12-10 16:14:56 -0800

    trying to swtich to AJAX searching function

---
+++ HW6/WorldWideImporter/WorldWideImporter/Controllers/AJAXController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorldWideImporter.Models;
+
+namespace WorldWideImporter.Controllers
+{
+    public class AJAXController : Controller
+    {
+        private WWIContext db = new WWIContext();
+        // GET: AJAX
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Search(string name)
+        {
+
+            Debug.WriteLine(name);
+            
+
+            var jsonObj = db.People.Where(p => p.FullName.Contains(name)).ToList();
+
+
+            return Json(jsonObj, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ HW6/WorldWideImporter/WorldWideImporter/Scripts/AJAX.js
@@ -0,0 +1,59 @@
+﻿function spaceHit() {
+    $('#people-search').bind('keypress', function (e) {
+
+        //user hit a space bar
+        if (e.which === 32) {
+            giphySearch();
+            e.preventDefault;
+        }
+
+        //What to do when the space bar is pressed 
+        function giphySearch() {
+            //gets the value of the user input
+            var txt = $('#people-search').val();
+            //get ths last item that is typed 
+            var lastitem = txt.split(" ").pop();
+
+            
+
+
+            //routing it to my custome controller to send request 
+            var source = "AJAX/Search/" + txt; //Source
+
+
+            //check if word it not boring, if not send request, if so just append the text to the view
+          
+                $.ajax({
+                    type: "GET",
+                    dataType: "json",
+                    data: { "name": lastitem },
+                    url: source,
+                    success: showResult,
+                    error: errorOnAjax
+
+                });
+            
+          
+            
+        }
+    });
+}
+
+
+function showResult(data) {
+    console.log(data);
+}
+
+function errorOnAjax() {
+    console.log("Error on AJAX");
+}
+
+
+function main() {
+
+    alert("loaded!");
+    spaceHit();
+}
+
+/*Main method */
+$(document).ready(main);
