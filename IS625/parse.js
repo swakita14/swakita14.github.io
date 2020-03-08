2a253d2702fae31579a917cef5b2d0f68fb9059a
Shion Wakita <swakita14@wou.edu>
2020-03-08 14:40:14 -0700

    done with mini project

--- IS625/parse.js
+++ IS625/parse.js
@@ -1,21 +1,64 @@
 
 $(document).ready(function(){
-  
+
+    // Get the input from the UI, parse action happens on change 
     var input = document.querySelector('input[type="file"]');
     input.addEventListener('change', function(e){
     const reader = new FileReader()
 
-    /** Reader has finished loading and reading the file  */
-    reader.onload = function(){
+    // Reader has finished loading and reading the file //
+    reader.onload = function(e){
+      
+      // Assigning content result
+      var content = reader.result;
+      
+      // Split the variables up by commas 
+      var lines = content.split("\r")
 
-      /** Processing and parsing CSV file */
-      const lines = reader.result.split('\n').map(function (line){
-        return line.split(',')
-      })
+      // Initialize line content variable 
+      var lineContent = "";
 
-      console.log(lines);
+      // Iterate through each line
+      for (var count = 0; count < lines.length; count ++){
+
+        // Add the starting parenthesis
+        lineContent += "("
+
+        // Access each item 
+        var column = lines[count].split(",");   
+        
+        // Add each item to the varaible with proper formmatting 
+        for (var i = 0; i < column.length; i ++){
+          lineContent += "'" + column[i] + "',";
+        }
+
+        // Removing uncessary quotation marks 
+        lineContent = lineContent.slice(0, -1);
+
+        // Closing the line
+        lineContent += "),"
+
+        
+      }
+
+      // Remove last data since it was blank
+      lineContent = lineContent.slice(0, -7);
+
+      // Here for testing purpose
+      console.log(lineContent);
+
+      // Automatically download the file 
+      download(lineContent, "seedScript.sql" , "text/plain" );
+
     }
+
+    // Read text.... Is it necessary?
     reader.readAsText(input.files[0])
 
+
+
   }, false)
+
+  
+
 });
